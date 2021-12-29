4a60c827f937cb1e500cc237167b3aa44969be7c
Christian Calefati <71175246+ChristianCalefati@users.noreply.github.com>
2021-12-29 18:07:59 +0100

    src added

---
+++ src/test/java/com/cc/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
