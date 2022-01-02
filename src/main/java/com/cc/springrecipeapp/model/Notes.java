d2464b0fbfef2d6ebded78fcfa1fe6edf9a75178
ccale <c.calefati@gmail.com>
2022-01-02 17:23:20 +0100

    - added Notes JPA entity - added Recipe JPA Entity

---
+++ src/main/java/com/cc/springrecipeapp/model/Notes.java
@@ -0,0 +1,40 @@
+package com.cc.springrecipeapp.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
