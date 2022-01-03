692e437418106a18e91711a2e6e2dcbe4879adf4
ccale <c.calefati@gmail.com>
2022-01-03 22:04:58 +0100

    - Add Enum Difficulty - Added Difficulty as Embedded property of Recipe specifing EnumType.STRING

---
+++ src/main/java/com/cc/springrecipeapp/model/Difficulty.java
@@ -0,0 +1,7 @@
+package com.cc.springrecipeapp.model;
+
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
--- src/main/java/com/cc/springrecipeapp/model/Recipe.java
+++ src/main/java/com/cc/springrecipeapp/model/Recipe.java
@@ -16,8 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -114,5 +115,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
